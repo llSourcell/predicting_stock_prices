a4c51faa34ade04be10222501a5bc67aaadcc6af
Siraj Raval <sirajraval1@gmail.com>
2016-10-28 15:13:25 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Regression-Stock-Prediction
+Predicting Google’s stock price using various regression techniques. Toy example for learning how to combine numpy, scikit-learn and matplotlib. Work to do.
+
+Based on [this](http://beancoder.com/linear-regression-stock-prediction/) tutorial.
---
+++ svr.py
@@ -0,0 +1,49 @@
+import csv
+import numpy as np
+from sklearn.svm import SVR
+import matplotlib.pyplot as plt
+
+
+plt.switch_backend('newbackend')  
+
+
+
+dates = []
+prices = []
+
+def get_data(filename):
+	with open(filename, 'r') as csvfile:
+		csvFileReader = csv.reader(csvfile)
+		next(csvFileReader)	# skipping column names
+		for row in csvFileReader:
+			dates.append(int(row[0].split('-')[0]))
+			prices.append(float(row[1]))
+	return
+
+def predict_price(dates, prices, x):
+	dates = np.reshape(dates,(len(dates), 1)) # converting to matrix of n X 1
+
+	svr_lin = SVR(kernel= 'linear', C= 1e3)
+	svr_poly = SVR(kernel= 'poly', C= 1e3, degree= 2)
+	svr_rbf = SVR(kernel= 'rbf', C= 1e3, gamma= 0.1) # defining the support vector regression models
+	svr_rbf.fit(dates, prices) # fitting the data points in the models
+	svr_lin.fit(dates, prices)
+	svr_poly.fit(dates, prices)
+
+	plt.scatter(dates, prices, color= 'black', label= 'Data') # plotting the initial datapoints 
+	plt.plot(dates, svr_rbf.predict(dates), color= 'red', label= 'RBF model') # plotting the line made by the RBF kernel
+	plt.plot(dates,svr_lin.predict(dates), color= 'green', label= 'Linear model') # plotting the line made by linear kernel
+	plt.plot(dates,svr_poly.predict(dates), color= 'blue', label= 'Polynomial model') # plotting the line made by polynomial kernel
+	plt.xlabel('Date')
+	plt.ylabel('Price')
+	plt.title('Support Vector Regression')
+	plt.legend()
+	plt.show()
+
+	return svr_rbf.predict(x)[0], svr_lin.predict(x)[0], svr_poly.predict(x)[0]
+
+get_data('goog.csv') # calling get_data method by passing the csv file to it
+#print "Dates- ", dates
+#print "Prices- ", prices
+
+predicted_price = predict_price(dates, prices, 29)  
