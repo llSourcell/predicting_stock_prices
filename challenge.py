5c6d95dda3246677fbb71bcb839962009a810c7d
Siraj <sirajravel@gmail.com>
2016-10-28 15:46:00 -0700

    Update challenge.py

--- challenge.py
+++ challenge.py
@@ -2,6 +2,8 @@ import tweepy
 import csv
 import numpy as np
 from textblob import TextBlob
+from keras.models import Sequential
+from keras.layers import Dense
 
 
 #Step 1 - Insert your API keys
@@ -13,13 +15,13 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
-#Step 2 - Search for the company name on Twitter
+#Step 2 - Search for your company name on Twitter
 public_tweets = api.search('company_name')
 
 
 #Step 3 - Define a threshold for each sentiment to classify each 
 #as positive or negative. If the majority of tweets you've collected are positive
-#then use your neural network to predict the next price
+#then use your neural network to predict a future price
 for tweet in public_tweets:    
     analysis = TextBlob(tweet.text)
     print(analysis.sentiment)
@@ -28,7 +30,6 @@ for tweet in public_tweets:
 #data collection
 dates = []
 prices = []
-
 def get_data(filename):
 	with open(filename, 'r') as csvfile:
 		csvFileReader = csv.reader(csvfile)
@@ -37,10 +38,9 @@ def get_data(filename):
 			dates.append(int(row[0].split('-')[0]))
 			prices.append(float(row[1]))
 	return
-
 get_data('aapl.csv')
 
-#Step 5 Build your neural network model using Keras, train it, then have it predict the price 
+#Step 5 In this function, build your neural network model using Keras, train it, then have it predict the price 
 #on a given day. We'll later print the price out to terminal.
 def predict_prices(dates, prices, x):
 
