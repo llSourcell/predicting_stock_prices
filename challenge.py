6bcac3319dc7b357574c32b1be36c2ff2d22660e
Siraj <sirajravel@gmail.com>
2016-10-28 15:42:45 -0700

    challenge.py

---
+++ challenge.py
@@ -0,0 +1,50 @@
+import tweepy
+import csv
+import numpy as np
+from textblob import TextBlob
+
+
+#Step 1 - Insert your API keys
+consumer_key= 'CONSUMER_KEY_HERE'
+consumer_secret= 'CONSUMER_SECRET_HERE'
+access_token='ACCESS_TOKEN_HERE'
+access_token_secret='ACCESS_TOKEN_SECRET_HERE'
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+#Step 2 - Search for the company name on Twitter
+public_tweets = api.search('company_name')
+
+
+#Step 3 - Define a threshold for each sentiment to classify each 
+#as positive or negative. If the majority of tweets you've collected are positive
+#then use your neural network to predict the next price
+for tweet in public_tweets:    
+    analysis = TextBlob(tweet.text)
+    print(analysis.sentiment)
+    
+
+#data collection
+dates = []
+prices = []
+
+def get_data(filename):
+	with open(filename, 'r') as csvfile:
+		csvFileReader = csv.reader(csvfile)
+		next(csvFileReader)
+		for row in csvFileReader:
+			dates.append(int(row[0].split('-')[0]))
+			prices.append(float(row[1]))
+	return
+
+get_data('aapl.csv')
+
+#Step 5 Build your neural network model using Keras, train it, then have it predict the price 
+#on a given day. We'll later print the price out to terminal.
+def predict_prices(dates, prices, x):
+
+predicted_price = predict_price(dates, prices, 29)
+print(predicted_price)
+
+
