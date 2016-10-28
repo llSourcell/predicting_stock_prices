faba5a770f1389d56c6d13399d0d9743e1394416
Siraj <sirajravel@gmail.com>
2016-10-28 15:15:31 -0700

    Update and rename svr.py to demo.py

--- demo.py
+++ demo.py
@@ -42,7 +42,7 @@ def predict_price(dates, prices, x):
 
 	return svr_rbf.predict(x)[0], svr_lin.predict(x)[0], svr_poly.predict(x)[0]
 
-get_data('goog.csv') # calling get_data method by passing the csv file to it
+get_data('aapl.csv') # calling get_data method by passing the csv file to it
 #print "Dates- ", dates
 #print "Prices- ", prices
 
